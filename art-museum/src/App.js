2ea74e0e5c92105bb76e498e5694d3d31527281e
Rohan Mudumba <roh20rocks@gmail.com>
2023-06-02 14:37:33 -0700

    Phase 1 finish

--- art-museum/src/App.js
+++ art-museum/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h1>Hello from App</h1>
-      {/* <GalleryNavigation galleries={harvardArt.records} /> */}
+      <GalleryNavigation galleries={harvardArt.records} />
     </>
   );
 }
--- art-museum/src/components/GalleryNavigation/index.js
+++ art-museum/src/components/GalleryNavigation/index.js
@@ -1,16 +1,17 @@
 import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
-
+import "./GalleryNavigation.css";
 function GalleryNavigation ({galleries}) {
     return (
         <nav>
-            < allLinks galleries={galleries}/>
+            <NavLink exact to="/">Home</NavLink>
+            < AllLinks galleries={galleries}/>
         </nav>
     )
 }
 
-const allLinks = ({galleries}) => {
+const AllLinks = ({galleries}) => {
     return galleries.map((gallery) => {
-        return(<NavLink>{gallery["name"]}</NavLink>)
+        return(<NavLink to={`/galleries/${gallery.id}`} key={ gallery.id }>{gallery["name"]}</NavLink>)
     })
 }
 
